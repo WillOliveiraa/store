b40022f00c99bed4133d7a7fd645f4b362003ded
willian oliveira <willian.lemes@iatec.com>
2021-05-21 11:53:25 -0300

    Setup Tests

---
+++ Store.Tests/Entities/OrderTests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Store.Tests.Domain
+{
+    [TestClass]
+    public class OrderTests
+    {
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_novo_pedido_valido_ele_deve_gerar_um_numero_com_8_caracteres()
+        {
+            Assert.Fail();
+        }
+    }
+}
