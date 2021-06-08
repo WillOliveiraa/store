65aa4b3073c1f1922671c9fc3f9803af5263c294
willian oliveira <willian.lemes@iatec.com>
2021-06-08 16:27:39 -0300

    Setup and paymant test

--- Store.Tests/Entities/OrderTests.cs
+++ Store.Tests/Entities/OrderTests.cs
@@ -11,22 +11,35 @@ namespace Store.Tests.Domain
         private readonly Customer _customer = new Customer("Willian Oliveira", "will@teste.com");
         private readonly Product _product = new Product("Teclado Gamer", 350, true);
         private readonly Discount _discount = new Discount(10, DateTime.Now.AddDays(5));
-        
+        private Order _order;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _order = new Order(_customer, 15, _discount);
+        }
+
         [TestMethod]
         [TestCategory("Domain")]
         public void Dado_um_novo_pedido_valido_ele_deve_gerar_um_numero_com_8_caracteres()
         {
-            var order = new Order(_customer, 15, _discount);
-            
-            Assert.AreEqual(8, order.Number.Length);
+            Assert.AreEqual(8, _order.Number.Length);
         }
 
         [TestMethod]
         [TestCategory("Domain")]
         public void Dado_um_novo_pedido_seu_status_deve_ser_aguardando_pagamento()
         {
-            var order = new Order(_customer, 15, _discount);
-            Assert.AreEqual(order.Status, EOrderStatus.WaitingPayment);
+            Assert.AreEqual(_order.Status, EOrderStatus.WaitingPayment);
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_pagamento_do_pedido_seu_status_deve_ser_aguardando_entrega()
+        {
+            _order.AddItem(_product, 1);
+            _order.Pay(355);
+            Assert.AreEqual(_order.Status, EOrderStatus.WaitingDelivery);
         }
     }
 }
